2f2b11af18357daeadd14fd2c83cf4d68a00c763
root <root@ASTINF-269336.cbmse.local>
2025-10-23 10:38:47 -0300

    first commit

---
+++ resources/views/admin/users/create.blade.php
@@ -0,0 +1,9 @@
+<h1>Criar usuário</h1>
+
+<form action={{ route('users.store') }} method="POST">
+    @csrf()
+    <input type="text" name="name" placeholder="Insira o nome">
+    <input type="email" name="email" placeholder="Insira o e-mail">
+    <input type="password" name="password" placeholder="Insira a senha"><br>
+    <button type="submit">Enviar</button>
+</form>
---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/products',
+    [\App\Http\Controllers\Admin\ProductController::class, 'index'])->name('produtos.index');
+
+// chama o controlador, que por sua vez executa a action (função), ex: index ou create
+Route::get('/users',
+    [\App\Http\Controllers\Admin\UserController::class, 'index'])->name('users.index');
+Route::get('/users/create',
+    [\App\Http\Controllers\Admin\UserController::class, 'create'])->name('users.create');
+Route::post('/users/create',
+    [\App\Http\Controllers\Admin\UserController::class, 'store'])->name('users.store');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
