cd77e869619ba272a03b1f44d40ee764c3e57c95
rodrigo <rodrigo.aragao13@gmail.com>
2025-10-23 11:40:50 -0300

    fim aula 09

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -1,11 +1,18 @@
-<h1>Criar usuário</h1>
+@extends('admin.layouts.app')
 
-<form action={{ route('users.store') }} method="POST">
-    @csrf()
-    <input type="text" name="name" placeholder="Insira o nome">
-    <input type="email" name="email" placeholder="Insira o e-mail">
-    <input type="password" name="password" placeholder="Insira a senha"><br>
-    <button type="submit">Enviar</button>
+@section('title', 'Criar Usuário')
+@section('content')
+    <h1>Criar usuário</h1>
 
-{{--    teste!--}}
-</form>
+    {{--    teste!--}}
+
+    <form action={{ route('users.store') }} method="POST">
+        @csrf()
+        <input type="text" name="name" placeholder="Insira o nome">
+        <input type="email" name="email" placeholder="Insira o e-mail">
+        <input type="password" name="password" placeholder="Insira a senha"><br>
+        <button type="submit">Enviar</button>
+
+        {{--    teste!--}}
+    </form>
+@endsection
--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -1,36 +1,31 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Usuários</h1>
-    <a href={{ route('users.create') }}>Novo usuário</a>
-    <table>
-        <thead>
-        <tr>
-            <th>Nome</th>
-            <th>E-mail</th>
-            <th>Ações</th>
-        </tr>
-        </thead>
-        <tbody>
-            @forelse($users as $user)
-                <tr>
-                    <td>{{$user->name}}</td>
-                    <td>{{$user->email}}</td>
-                    <td>-</td>
-                </tr>
-            @empty
-                <tr>
-                    <td colspan="50">Nenhum usuário encontrado.</td>
-                </tr>
-            @endforelse
-        </tbody>
-    </table>
+@extends('admin.layouts.app')
 
-    {{ $users->links() }}
-</body>
-</html>
+@section('title', 'Lista de Usuários')
+@section('content')
+<h1>Usuários</h1>
+<a href={{ route('users.create') }}>Novo usuário</a>
+<table>
+    <thead>
+    <tr>
+        <th>Nome</th>
+        <th>E-mail</th>
+        <th>Ações</th>
+    </tr>
+    </thead>
+    <tbody>
+    @forelse($users as $user)
+        <tr>
+            <td>{{$user->name}}</td>
+            <td>{{$user->email}}</td>
+            <td>-</td>
+        </tr>
+    @empty
+        <tr>
+            <td colspan="50">Nenhum usuário encontrado.</td>
+        </tr>
+    @endforelse
+    </tbody>
+</table>
+
+{{ $users->links() }}
+@endsection
